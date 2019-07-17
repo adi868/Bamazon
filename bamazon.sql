ade463a38085e68653254aea6520689a8bc433f6
alciancio <alcianciotto@gmail.com>
2019-07-17 16:11:12 -0500

    Updates to DB and javascript. Updating inventory, inquiries, text outputs working

--- bamazon.sql
+++ bamazon.sql
@@ -18,38 +18,35 @@ stock_quantity INTEGER(10) NOT NULL
 
 -- Creates new rows containing data in all named columns --
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5000, "Apple", "Produce", 1.25, 30);
+VALUES (5000, "Apple", "Produce", 1.25, 60);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5001, "Banana", "Produce", .99, 20);
+VALUES (5001, "Banana", "Produce", .99, 50);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5002, "Orange", "Produce", 1.35, 10);
+VALUES (5002, "Orange", "Produce", 1.35, 30);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5003, "Pineapple", "Produce", 2.29, 5);
+VALUES (5003, "Pineapple", "Produce", 2.29, 8);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5004, "Watermelon", "Produce", 3.39, 2);
+VALUES (5004, "Watermelon", "Produce", 3.39, 20);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5005, "Cherry", "Produce", 1.19, 25);
+VALUES (5005, "Cherry", "Produce", 1.19, 40);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
 VALUES (5006, "Magical Gum-Gum Fruit", "Produce", 999.99, 1);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5007, "Blueberry", "Produce", 1.59, 12);
+VALUES (5007, "Blueberry", "Produce", 1.59, 20);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5008, "Lemon", "Produce", 1.89, 10);
+VALUES (5008, "Grapefruit", "Produce", 1.89, 36);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5009, "Kiwi", "Produce", 2.59, 5);
+VALUES (5009, "Kiwi", "Produce", 2.59, 25);
 
 SELECT * FROM products;
 
--- update products
--- SET stock_quantity = 
--- WHERE product_name = ;
  
--- test.js
+++ test.js
@@ -2,7 +2,6 @@ var inquirer = require("inquirer");
 const chalk = require("chalk");
 var mySQL = require("mysql");
 var Table = require("cli-table3");
-require('dotenv').config();
 
 var connection = mySQL.createConnection({
     host: "127.0.0.1",
@@ -14,27 +13,48 @@ var connection = mySQL.createConnection({
 connection.connect(function (error) {
     if (error) throw error;
     console.log("Connected as ID:", connection.threadId + "\n");
-    inventoryItem();
+    optionMenu();
 });
+
+function optionMenu() {
+    console.log((chalk.green("Welcome to Bamazon!")))
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: ["Make a purchase", "Exit"]
+    }).then(function (response) {
+        switch (response.choice) {
+            case "Make a purchase":
+                inventoryItem();
+                break;
+            case "Exit":
+                connection.end();
+                console.log("You have exited the program. Please come again!")
+                break;
+        }
+    })
+}
 
 function inventoryItem() {
     var query = "SELECT * FROM products";
     connection.query(query, function (error, response) {
         if (error) throw error;
         var divider = "\n------------------------------------------------------------\n";
-        var greeting = ((chalk.green("Welcome to Bamazon!")) + "\n" + "Here is the current available inventory:" + "\n")
+        var greeting = "\n" + "Here is the current available inventory:" + "\n"
         console.log(greeting);
         var table = new Table({
-            head: ["ID", "Item", "Price"]
+            head: ["ID", "Item", "Price", "In Stock"]
         });
         for (var i = 0; i < response.length; i++) {
             table.push([
                 response[i].item_id,
                 response[i].product_name,
-                '$'+ response[i].price
-            ])
+                '$' + response[i].price,
+                response[i].stock_quantity
+            ]);
         }
-        console.log(table.toString());
+        console.log(table.toString() + "\n");
         runProgram();
     })
 }
@@ -44,11 +64,17 @@ function runProgram() {
         .prompt([{
                 type: "input",
                 message: "\n" + "Please enter the ID of the product you would like to buy:",
-                name: "itemId"
+                name: "itemId",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
             },
             {
                 type: "input",
-                message: "How many units of the product would you like to buy?",
+                message: "How many would you like to buy?",
                 name: "units",
                 validate: function (value) {
                     if (isNaN(value) === false) {
@@ -57,28 +83,31 @@ function runProgram() {
                     return false;
                 }
 
+            },
+            {
+                type: "confirm",
+                message: "Is this correct?",
+                name: "confirmation",
+                default: true
             }
         ]).then(function (userResponse) {
-            var query = "SELECT * FROM products";
-            connection.query(query, function (error, response) {
-                if (error) throw error;
+            connection.query("SELECT * FROM products WHERE ?", {
+                item_id: userResponse.itemId
+            }, function (error, response) {
+                console.log("\nYou have chosen to buy " + userResponse.units + " " + response[0].product_name + "(s).");
+                if (userResponse.units > response[0].stock_quantity) {
+                    console.log("\n" + "Oh no. We do not have enough in stock! We are sorry.")
+                } else {
+                    console.log("\nProcessing your order....\n");
+                    var totalCost = userResponse.units * response[0].price;
+                    var updateStock = response[0].stock_quantity - userResponse.units;
+                    var update = "UPDATE products SET stock_quantity = " + updateStock + " WHERE item_id = " + userResponse.itemId;
+                    connection.query(update, function (error, response) {
+                        if (error) throw error
+                        console.log("Transaction complete! The total cost of your purchase was: $" + totalCost + ".")
+                        console.log("Please come back again. :)")
+                    })
+                }
             })
         });
-
 }
-
-
-// function IDSearch(){
-// var query = "SELECT item_id FROM products"
-// connection.query(query)
-// }
-
-// function unitSearch(){
-// var unitQuery = "SELECT stock_quantity FROM products"
-
-// }
-
-//     var unit = userResponse.units
-//     console.log("You have chosen" + product);
-//     console.log("You would like" + unit + "units");
-//     console.log("Please wait while we check out inventory.")
