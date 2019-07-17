15c2ae2bd1f7d7fe74d0a63d386764f89da0c5be
alciancio <alcianciotto@gmail.com>
2019-07-17 12:20:10 -0500

    database updated and work on js

--- bamazon.sql
+++ bamazon.sql
@@ -8,48 +8,46 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 -- creates the table "products" within bamazon_db
-CREATE TABLE products(
-item_id INTEGER(10),
-product_name VARCHAR(50),
-department_name VARCHAR(50),
-price INTEGER(10),
-stock_quantity INTEGER(10)
+CREATE TABLE products (
+item_id INTEGER(10) NOT NULL,
+product_name VARCHAR(50) NOT NULL,
+department_name VARCHAR(50) NOT NULL,
+price DECIMAL(10,2),
+stock_quantity INTEGER(10) NOT NULL
 );
 
 -- Creates new rows containing data in all named columns --
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Apple", "Produce", 1.25, 40);
+VALUES (5000, "Apple", "Produce", 1.25, 30);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Banana", "Produce", 1.25, 40);
+VALUES (5001, "Banana", "Produce", .99, 20);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Orange", "Produce", 1.25, 40);
+VALUES (5002, "Orange", "Produce", 1.35, 10);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Pineapple", "Produce", 1.25, 40);
+VALUES (5003, "Pineapple", "Produce", 2.20, 5);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Watermelon", "Produce", 1.25, 40);
+VALUES (5004, "Watermelon", "Produce", 3.30, 2);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Cherry", "Produce", 1.25, 40);
+VALUES (5005, "Cherry", "Produce", 1.10, 25);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Tomato", "Produce", 1.25, 40);
+VALUES (5006, "Magical Gum-Gum Fruit", "Produce", 999.99, 1);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Apple", "Produce", 1.25, 40);
+VALUES (5007, "Blueberry", "Produce", 1.50, 12);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Apple", "Produce", 1.25, 40);
+VALUES (5008, "Lemon", "Produce", 1.80, 10);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Apple", "Produce", 1.25, 40);
+VALUES (5009, "Kiwi", "Produce", 2.50, 5);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2000, "Apple", "Produce", 1.25, 40);
-
+SELECT * FROM products;
 
 -- update products
 -- SET stock_quantity = 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,23 +1,69 @@
 var inquirer = require("inquirer");
 const chalk = require("chalk");
-var mysql = require("mysql");
+var mySQL = require("mysql");
 
-inquirer
-.prompt([{
-    type: "input",
-    message: (chalk.blue("Welcome to Bamazon.")) + "\n" + "Please enter the ID of the product you would like to buy:",
-    name: "itemId"
-},
-{
-    type: "input",
-    message:"How many units of the product would you like to buy?",
-    name: "units"
-}
-]).then(function(userResponse){
-    if(userResponse ==true){
-    console.log("Ok!")
-}
-else{
-    console.log("Insufficient quantity! Your order cannot go through.")
-}
+var connection = mySQL.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_db"
 });
+connection.connect(function (error) {
+    if (error) throw error;
+    console.log("We have an error", connection.threadId);
+})
+
+console.log("test")
+
+// function showItems() {
+//     varItems = "SELECT * FROM products"
+// }
+// showItems();
+
+// inquirer
+//     .prompt([{
+//             type: "input",
+//             message: (chalk.blue("Welcome to Bamazon.")) + "\n" + "Please enter the ID of the product you would like to buy:",
+//             name: "itemId"
+//         },
+//         {
+//             type: "input",
+//             message: "How many units of the product would you like to buy?",
+//             name: "units"
+//         }
+//     ]).then(function (userResponse) {
+//         var product = "SELECT item_id FROM products";
+//         connection.query(query, {
+//             product_name: answer.product_name
+//         }, function (error, response) {
+//             for (var i = 0; i < response.length; i++) {
+//                 if (userResponse.item_Id === product) {
+//                     console.log("Cool! We got a match.")
+//                 } else {
+//                     console.log("Nope. Didn't work.")
+//                 }
+//             }
+//         })
+//     });
+
+
+// function IDSearch(){
+// var query = "SELECT item_id FROM products"
+// connection.query(query)
+// }
+
+// function unitSearch(){
+// var unitQuery = "SELECT stock_quantity FROM products"
+
+// }
+
+//     var unit = userResponse.units
+//     console.log("You have chosen" + product);
+//     console.log("You would like" + unit + "units");
+//     console.log("Please wait while we check out inventory.")
+
+
+//      if(userResponse.itemId ==true){
+//      console.log("Ok!")
+//  }
